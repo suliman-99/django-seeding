5397781a07b97fde38b6d04e4bf47af7285890f0
Suliman Awad <sulimanawadstudy@gmail.com>
2023-10-24 22:56:11 +0300

    add description to the package for pypi search

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 setuptools.setup(
     name='django-seeding',
-    version='1.0.4',
+    version='1.0.6',
+    description='Simple Django Package that helps developer to seed data from files and codes into the database automatically',
     long_description=Path('README.md').read_text(),
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(include=['django_seeding']),
