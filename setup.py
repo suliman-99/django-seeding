7ed7d82d9e717dcc6c990b8a6a82ee81bf889b4c
Suliman Awad <sulimanawadstudy@gmail.com>
2023-10-25 19:47:59 +0300

    Fix github action error

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 import setuptools
-import subprocess
 
-version = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
 
 setuptools.setup(
-    version=version,
     packages=setuptools.find_packages(include=['django_seeding']),
     install_requires=('django', 'djangorestframework', 'pandas'),
 )
