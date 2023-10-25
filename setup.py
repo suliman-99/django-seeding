c46a61a7f297530edc73beab5b6750e5273859f3
Suliman Awad <sulimanawadstudy@gmail.com>
2023-10-25 16:24:30 +0300

    set the configurations in pyproject.toml file

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 import setuptools
-from pathlib import Path
 
 
 setuptools.setup(
-    name='django-seeding',
-    version='1.0.9',
-    description='Simple Django Package that helps developer to seed data from files and codes into the database automatically',
-    long_description=Path('README.md').read_text(),
-    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(include=['django_seeding']),
-    install_requires=('django', 'djangorestframework', 'pandas')
 )
