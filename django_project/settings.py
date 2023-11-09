f6bc31a96b99d4c679895c45d08481ee4f9dd505
Suliman Awad <sulimanawadstudy@gmail.com>
2023-11-09 12:42:59 +0300

    fix commands error

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_seeding',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_seeding',
     'django_seeding_example',
 ]
 
