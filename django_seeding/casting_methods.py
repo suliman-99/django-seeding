0d2da1e89b9af3911c023f6513ecdc1ac85e28f9
Suliman Awad <sulimanawadstudy@gmail.com>
2024-04-22 14:46:15 +0300

    fix a bug

--- django_seeding/casting_methods.py
+++ django_seeding/casting_methods.py
@@ -1,8 +1,9 @@
 
 
 def cast_to_bool(text: str):
-    if text.lower() in ['true', '1', 't', 'y', 'yes']:
-        return True
-    elif text.lower() in ['false', '0', 'f', 'n', 'no']:
-        return False
+    if isinstance(text, str):
+        if text.lower() in ['true', '1', 't', 'y', 'yes']:
+            return True
+        elif text.lower() in ['false', '0', 'f', 'n', 'no']:
+            return False
     return None
