0fb0d8dea9f5117b4737c11840a01aab55ba4194
Suliman Awad <sulimanawadstudy@gmail.com>
2024-09-18 10:39:53 +0300

    add created_at and updated_at datetime fields to AppliedSeeder Model

--- django_seeding/admin.py
+++ django_seeding/admin.py
@@ -4,5 +4,18 @@ from django_seeding.models import AppliedSeeder
 
 @admin.register(AppliedSeeder)
 class AppliedSeederAdmin(admin.ModelAdmin):
-    list_display = ('id', )
-    search_fields = ('id', )
+    list_display = (
+        'id',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'created_at',
+        'updated_at',
+    )
+    search_fields = (
+        'id',
+    )
+    ordering = (
+        '-updated_at',
+    )
--- django_seeding/models.py
+++ django_seeding/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class AppliedSeeder(models.Model):
     id = models.CharField(max_length=100, primary_key=True)
+    
+    created_at = models.DateTimeField(null=True, blank=True, auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True, auto_now=True)
 
     def __str__(self) -> str:
         return self.id
